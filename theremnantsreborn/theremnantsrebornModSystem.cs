83e922caa9ae5d0df88c3d3f1c31e211169cf9ed
Tyeia <Tyeia6633@gmail.com>
2026-07-11 00:34:59 -0400

    Specific woodsman log dupe trait works, want to generalize tho

--- theremnantsreborn/theremnantsrebornModSystem.cs
+++ theremnantsreborn/theremnantsrebornModSystem.cs
@@ -1,7 +1,14 @@
 ﻿using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
+using Vintagestory.GameContent;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
 
 namespace theremnantsreborn;
 
 public class theremnantsrebornModSystem : ModSystem
 {
 }
+
